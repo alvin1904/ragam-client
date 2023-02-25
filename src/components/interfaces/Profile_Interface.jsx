3b5b0b8dc36a1c3ec4f94abfb52e2c5f247831a1
alvin1904 <alvin19official@gmail.com>
2023-02-25 13:39:14 +0530

    feat: complete profile and settings pages

--- src/components/interfaces/Profile_Interface.jsx
+++ src/components/interfaces/Profile_Interface.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./Profile.css";
 
 export default function Profile_Interface() {
+  const details = {
+    name: "Alvin Varghese",
+    email: "alvin19official@gmail.com",
+    verified: false,
+  };
+
   return (
-    <div className="interface_inside">Profile_Interface</div>
-  )
+    <div className="interface_inside">
+      <div className="profile_intf">
+        <div className="profile_section">
+          <h1 className="profile_welcome_main">
+            Hey {details.name.substring(0, details.name.indexOf(" "))},
+          </h1>
+          <div className="profile_subsection">
+            <h1 className="profile_welcome">Profile Details</h1>
+            <span>Name:</span>
+            <span className="p"> {details.name}</span>
+            <br></br>
+            <span>Email:</span>
+            <span className="p"> {details.email}</span>
+          </div>
+          <div className="profile_subsection">
+            <h1 className="profile_welcome">Profile Verification</h1>
+            <span>Status:</span>
+            <span className="p">
+              {details.verified ? "VERIFIED :)" : "NOT VERIFIED"}
+            </span>
+            <br></br>
+            {!details.verified && (
+              <span>
+                Go to inbox and click on VERIFY to start enjoying our services.
+                :)
+              </span>
+            )}
+          </div>
+          <div className="profile_subsection">
+            <h1 className="profile_welcome">Change username / password</h1>
+            <span>
+              Go over to <span className="p">SETTINGS</span> to update username or
+              password
+            </span>
+          </div>
+        </div>
+        <div className="profile_section playlist_section">
+          <h1 className="profile_welcome">Playlists created by you</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
