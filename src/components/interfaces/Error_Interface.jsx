013b49e34575325ca7dd3616f0ed7b02a5e943a6
alvin1904 <alvin19official@gmail.com>
2023-02-25 07:42:37 +0530

    feat: complete loading & error interfaces

--- src/components/interfaces/Error_Interface.jsx
+++ src/components/interfaces/Error_Interface.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Error_Interface() {
   return (
-    <div className='interface_inside'>Error_Interface</div>
-  )
+    <div className="interface_inside">
+      <div className="error_intf">
+        <iframe
+          src="https://giphy.com/embed/3o85xn8b6TuGGTnSOk"
+          width="280"
+          height="262"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+        <p>
+          <a href="https://giphy.com/gifs/3o85xn8b6TuGGTnSOk"></a>
+        </p>
+        Something's not right!
+      </div>
+    </div>
+  );
 }
--- src/components/interfaces/Loading_Interface.jsx
+++ src/components/interfaces/Loading_Interface.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Loading_Interface() {
   return (
-    <div className="interface_inside">Loading_Interface</div>
-  )
+    <div className="interface_inside">
+      <div className="error_intf">
+        <iframe
+          src="https://giphy.com/embed/3y0oCOkdKKRi0"
+          width="380"
+          height="250"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+        <p>
+          <a href="https://giphy.com/gifs/3y0oCOkdKKRi0"></a>
+        </p>
+        It's cooking. Have patience!
+      </div>
+    </div>
+  );
 }
